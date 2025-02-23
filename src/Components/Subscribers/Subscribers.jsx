f63e9e4edcb87f34339656238407edd0b26b59bd
Alexey Borisov <ablogmount@yandex.ru>
2025-02-23 09:52:33 +0500

    Fix/ Del no use cod

--- src/Components/Subscribers/Subscribers.jsx
+++ src/Components/Subscribers/Subscribers.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import styles from "./Subscribers.module.css";
-
-// const Subscribers = () => {
-//   return (
-//     <section className={`${styles.subscribe} section`}>
-//       <div className={`${styles.sectionContainer} container`}>
-//         <h2>Subscribe newsletters & get Latest news</h2>
-//         <div className={`${styles.inputDiv} flex`}>
-//           <input type="text" placeholder="Enter your Email adress" />
-//           <button className={`btn ${styles.btn}`}>Subscribe</button>
-//         </div>
-//       </div>
-//     </section>
-//   );
-// };
-
-// export default Subscribers;
-
-
 import React from "react";
 import styles from "./Subscribers.module.css";
 
